9af3a22790a64b01284c729dd6b443e841fbce30
Eldritch Cheese <eldritch.cheese@gmail.com>
2020-11-15 13:56:51 -0800

    Updated emacs install script with packages needed.

--- emacs.sh
+++ emacs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-VERSION=24.5
-PREFIX=~/bin/emacs-${VERSION}
+# sudo apt install libxaw7-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev gnutls-dev libncurses-dev
+
+VERSION=27.1
+#PREFIX=~/bin/emacs-${VERSION}
+PREFIX=/opt/emacs-${VERSION}
 
 wget http://ftpmirror.gnu.org/emacs/emacs-${VERSION}.tar.gz
 tar -xzf emacs-${VERSION}.tar.gz
 rm -f emacs-${VERSION}.tar.gz
 cd emacs-${VERSION}
 ./configure --prefix ${PREFIX} --with-x-toolkit=lucid
-make -j10 && make install
+make -j10
+sudo make install
