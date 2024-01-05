8b45e0d31a5f52faa9cd31fcba83ea5e6ca4ba51
Eric Lunderberg <elunderberg@octoml.ai>
2024-01-05 17:21:37 +0000

    Added ccache build script

---
+++ pybuild_scripts/ccache.py
@@ -0,0 +1,33 @@
+# -*- python -*-
+
+import pybuild
+
+
+class CCacheBuilder(pybuild.ProgramBuilder):
+    name = "ccache"
+
+    source_repository = "https://github.com/ccache/ccache.git"
+    git_tag_regex = r"^v(?P<version>\d+\.\d+\.\d+)$"
+
+    apt_build_dependencies = [
+        # Standard
+        "git",
+        "cmake",
+        "ninja-build",
+        "g++",
+        "gcc",
+        # Library specific
+        "zlib1g-dev",
+        "libzstd-dev",
+    ]
+
+    build_system = "cmake"
+    configure = [
+        "-DOFFLINE=TRUE",
+        "-DZSTD_FROM_INTERNET=OFF",
+        "-DCMAKE_BUILD_TYPE:STRING=Release",
+    ]
+
+    apt_run_dependencies = [
+        "libzstd1",
+    ]
